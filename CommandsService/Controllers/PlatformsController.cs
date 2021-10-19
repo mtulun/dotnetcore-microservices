ed22d65bca254dd760c748d41503c132dea25af5
Mustafa Taylan Ulun <taylan.ulun@outlook.com>
2021-10-20 01:47:34 +0300

    CommandsService Controllers

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data.Abstract;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -6,9 +10,23 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo repository;
+        private readonly IMapper mapper;
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
-            
+            this.mapper = mapper;
+            this.repository = repository;
+
+        }
+
+        /*                  MOVE ON HERE!!!!!                    */
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            System.Console.WriteLine("--> Getting platforms from CommandsService!");  
+            var platformItems = repository.GetAllPlatforms();
+            return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
 
         [HttpPost]
--- CommandsService/Dtos/PlatformReadDto.cs
+++ CommandsService/Dtos/PlatformReadDto.cs
@@ -2,6 +2,7 @@ namespace CommandsService.Dtos
 {
     public class PlatformReadDto
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
